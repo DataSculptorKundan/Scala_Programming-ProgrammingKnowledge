f13847f23e6e1b496b0316f4d0c205439aa4bfdf
Kundan Kumar <kkumar.cpr@gmail.com>
2023-01-01 13:41:12 +0530

    Added code for Scala Tuples

---
+++ src/main/scala/scala_Tuples.scala
@@ -0,0 +1,24 @@
+object scala_Tuples {
+  val myTuples: (Int, Int, String, Boolean, Float) = (1,2,"Hello",true, 225.05f)
+  val myTuples2: (Int, Boolean) = Tuple2(7, true)
+  val myTuples3: (Int, String, (Int, Int)) = Tuple3(9, "kundan", (1, 8))
+
+
+  def main(args: Array[String]): Unit = {
+    println(myTuples3)
+    println(myTuples2)
+    println(myTuples)
+
+    println(myTuples._1)
+    println(myTuples3._3)
+    println(myTuples3._3._2)
+
+    myTuples.productIterator.foreach{
+      itr => println(itr)
+    }
+
+    println(27 -> "Kundan") // Will create another tuple
+
+  }
+
+}
