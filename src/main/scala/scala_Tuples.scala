d4629a60d9dae8b6100473a304cb378f9f5cca3e
Kundan Kumar <kkumar.cpr@gmail.com>
2023-01-01 14:02:58 +0530

    Updated code for Scala Tuples

--- src/main/scala/scala_Tuples.scala
+++ src/main/scala/scala_Tuples.scala
@@ -1,7 +1,10 @@
 object scala_Tuples {
   val myTuples: (Int, Int, String, Boolean, Float) = (1,2,"Hello",true, 225.05f)
+  // val myTuples = (1,2,"Hello",true, 225.05f)
   val myTuples2: (Int, Boolean) = Tuple2(7, true)
+  //val myTuples2 = new Tuple2(7, true)
   val myTuples3: (Int, String, (Int, Int)) = Tuple3(9, "kundan", (1, 8))
+  //val myTuples3 = new Tuple3(9, "kundan", (1, 8))
 
 
   def main(args: Array[String]): Unit = {
